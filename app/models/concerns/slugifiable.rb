e0c27029bf08a8ed44c0383cb4ca0c75b5da0ab8
Olivia Kesler <oakesler@protonmail.ch>
2020-01-09 15:40:48 +0000

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,12 +1,14 @@
 module Slugifiable
 #module Slugifiable < Sinatra::Base
-  def slug
-    name = self.name.sub(/\s/, "-")
-    slug = name.sub(/[^\w-]+/, "").downcase
+  #class Slugifiable::Artist
+    def slug
+      name = self.name.sub(/\s/, "-")
+      slug = name.sub(/[^\w-]+/, "").downcase
+    end
+  
+    def find_by_slug(slug)
+      name = name.sub("/\s/", "-")
+      slug = name.delete(/\W/)
+    end
   end
-
-  def find_by_slug(slug)
-    name = name.sub("/\s/", "-")
-    slug = name.delete(/\W/)
-  end
-end
+#end
