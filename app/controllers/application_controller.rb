63ff1d333476c89a5c7be8166f55d4bc5ae3538c
Olivia Kesler <oakesler@protonmail.ch>
2020-01-09 18:10:15 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,16 +19,4 @@ class ApplicationController < Sinatra::Base
   get '/artists' do
     erb :artists
   end
-  
-  get '/songs/:slug' do
-    erb :"songs/:slug"
-  end
-  
-  get '/artists/:slug'
-    erb :"artists/:slug"
-  end
-  
-  get '/genres/:slug'
-    erb :"genres/:slug"
-  end
 end
